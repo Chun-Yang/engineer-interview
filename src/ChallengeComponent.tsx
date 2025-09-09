fa67ac555d8162457787f0f0e6159ab9eb7cac35
Chun-Yang <yang2007chun@gmail.com>
2025-09-09 16:04:39 -0400

    create item type and add logic for moving items around

--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -1,19 +1,40 @@
+import { useState } from 'react';
+
 enum ColumnType {
-  Todo,
-  InProgress,
-  Done,
+  Todo,       // 0
+  InProgress, // 1
+  Done,       // 2
+}
+
+type Item = {
+  id: number, // for now, id is timestamp `+new Date()`
+  content: string,
 }
 
 export function ChallengeComponent() {
+  const [itemsByColumn, setItemsByColumn] = useState({
+    [ColumnType.Todo]: [],
+    [ColumnType.InProgress]: [],
+    [ColumnType.Done]: [],
+  })
   const allColumnTypes = [
     ColumnType.Todo,
     ColumnType.InProgress,
     ColumnType.Done,
   ];
+
+  function handleMove(item: Item, columTypeFrom: ColumnType, columTypeTo: ColumnType ) {
+    setItemsByColumn({
+      ...itemsByColumn,
+      [columTypeFrom]: itemsByColumn[columTypeFrom].filter(i => i.id != item.id),
+      [columTypeTo]: itemsByColumn[columTypeTo].concat([item]),
+    });
+  }
+
   return (
     <>
       {allColumnTypes.map((columnType) => (
-        <Column columnType={columnType} key={columnType} />
+        <Column columnType={columnType} key={columnType} onMove={handleMove} />
       ))}
       <AddTask />
     </>
