c86b97da13fe9e6a49bdd46d8af7027a0f828244
Chun-Yang <yang2007chun@gmail.com>
2025-09-09 17:19:58 -0400

    disable invalid column change and apply css for disabled button

--- src/lib/Column.tsx
+++ src/lib/Column.tsx
@@ -1,4 +1,4 @@
-import {AllColumnTypes, ColumnType, Item} from './constants';
+import { AllColumnTypes, ColumnType, Item } from "./constants";
 
 export function Column({
   columnType,
@@ -42,9 +42,23 @@ export function Column({
             }}
             key={item.id}
           >
-            <button onClick={() => handleMoveWithDirection(item, -1)}>←</button>
+            <button
+              type="button"
+              disabled={columnType == ColumnType.Todo}
+              className="disabled:bg-gray-300 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              onClick={() => handleMoveWithDirection(item, -1)}
+            >
+              ←
+            </button>
             {item.content}
-            <button onClick={() => handleMoveWithDirection(item, 1)}>→</button>
+            <button
+              type="button"
+              disabled={columnType == ColumnType.Done}
+              className="disabled:bg-gray-300 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              onClick={() => handleMoveWithDirection(item, 1)}
+            >
+              →
+            </button>
           </div>
         );
       })}
@@ -62,4 +76,3 @@ function getTitleForColumn(columnType: ColumnType) {
       return "Done";
   }
 }
-
